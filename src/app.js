324131d5ac0118b6f83f014250350706b2155580
Jayash Saini <jayashysaini7361@gmail.com>
2024-05-29 16:43:36 +0530

    feat(Socket): Initialized web sockets using sokect io

--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const { Server } = require('socket.io');
+const http = require('http');
 
 const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CORS_ORIGIN || '*',
+    credentials: true,
+  },
+});
+
+app.set('io', io);
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN || '*',
@@ -23,4 +36,4 @@ app.use(helmet());
 // Log requests with Morgan middleware (use 'combined' format for production)
 app.use(morgan('dev'));
 
-module.exports = { app };
+module.exports = { httpServer };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const dotenv = require('dotenv');
-const { app } = require('./app.js');
+const { httpServer } = require('./app.js');
 
 dotenv.config({
   path: './.env',
 });
 
 (() => {
-  app.listen(process.env.PORT, () => {
+  httpServer.listen(process.env.PORT, () => {
     console.log(`🚝 Server is running on port ${process.env.PORT}`);
   });
 })();
