ed39956888b80a1937194ef4778edaf83bb3f05f
Jayash Saini <jayashysaini7361@gmail.com>
2024-05-29 17:07:19 +0530

    feat(App): Implement Utils, Error Handler, Connect mongoDb

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const DB_NAME = 'Cluster0';
+
+module.exports = { DB_NAME };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const dotenv = require('dotenv');
-const { httpServer } = require('./app.js');
-
-dotenv.config({
+require('dotenv').config({
   path: './.env',
 });
+const { httpServer } = require('./app.js');
+const connectDB = require('./db/index.js');
 
-(() => {
+(async () => {
+  // connect mongodb database
+  await connectDB();
+
+  // start http server
   httpServer.listen(process.env.PORT, () => {
     console.log(`🚝 Server is running on port ${process.env.PORT}`);
   });
